afb760e77836051249835b63c8e260914e5a183e
Rosen Blagoev <rossencho@mail.ru>
2017-08-13 21:05:36 +0300

    initial

--- app/ts/app.component.ts
+++
@@ -1,8 +0,0 @@
-import IComponentOptions = angular.IComponentOptions;
-
-class AppComponent {}
-
-export const appComponent: IComponentOptions = {
-    controller: AppComponent  ,
-    template:`<tasks></tasks>` 
-};
